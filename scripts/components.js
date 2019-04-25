6254e32aa38390c71dd4a98c171f11daaac3799a
adahab91 <adahaberg@Adas-MacBook-Pro.local>
2019-04-25 15:08:18 +0200

    added mtg page

--- scripts/components.js
+++ scripts/components.js
@@ -1,11 +1,28 @@
+//MOVE TO TOP ON CLICK
 let toTop = document.getElementById("to-top");
 
-toTop.addEventListener("click", moveToTop);
-
-function moveToTop(){
+toTop.addEventListener("click", function moveToTop(){
     window.scrollTo({
         top: 0,
         left: 0,
         behavior: 'smooth'
     });
-}
+});
+
+
+
+
+//TOGGLE HIDE/SHOW ON CLICK
+
+
+function toggleElement(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'block'){ 
+        e.style.display = 'none';
+        e.style.transition = "all 2s";
+    }
+    else {
+        e.style.display = 'block';
+        e.style.transition = "all 2s";
+    }
+ }
